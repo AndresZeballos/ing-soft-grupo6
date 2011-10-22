0357a5f46c17477fd4d835501d1784969940fb55
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@923e5f02-af3f-5d9a-ddc9-3f7426e0e9f8>
2011-10-22 01:22:52 +0000

    Comienzo del proyecto compartido

---
+++ trunk/SistemaDeEnvios/src/sistemadeenvios/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadeenvios;
+
+/**
+ *
+ * @author Andres
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login vp = new Login();
+        vp.setVisible(true);
+        vp.setDefaultCloseOperation(VentPrincipal.EXIT_ON_CLOSE);
+    }
+}
