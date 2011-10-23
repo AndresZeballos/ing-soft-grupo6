0e09975ffa355730cd8326a687607a5c35e4d2d8
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@923e5f02-af3f-5d9a-ddc9-3f7426e0e9f8>
2011-10-23 21:06:49 +0000

    Interfase a ser implementada por la clase usuario y por el stub del mismo.

---
+++ SistemaDeEnvios/src/sistemadeenvios/IUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sistemadeenvios;
+
+/**
+ *
+ * @author Andres
+ */
+public interface IUsuario {
+
+    public boolean validarAcceso(String componente);
+    
+}
