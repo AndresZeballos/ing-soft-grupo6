a69e9ee03f677f18674020a2feb5dc72bf143a8f
fedemartino@gmail.com <fedemartino@gmail.com@923e5f02-af3f-5d9a-ddc9-3f7426e0e9f8>
2011-10-26 19:28:32 +0000

    Perfiles de usuario y perfil Administrador

---
+++ SistemaDeEnvios/src/sistemadeenvios/stubs/UsuarioAdministrador.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sistemadeenvios.stubs;
+
+/**
+ *
+ * @author Fede
+ */
+public class UsuarioAdministrador implements sistemadeenvios.IUsuario{
+    public boolean validarAcceso(String componente)
+    {
+        return true;
+    }
+}
